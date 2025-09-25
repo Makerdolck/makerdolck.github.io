e6686745a4606c1fe03981cd50c436e762321824
Dmitriy <dmitry.dim-2011@yandex.ru>
2025-09-25 14:47:10 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = "calm-flow-v1";
+const ASSETS = ["/", "/index.html", "/icon.png", "/manifest.webmanifest"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
